22e2a108bde1f6e4251ce2d1387beffabf3f9aa5
Dimitrios Piliouras <jimpil1985@gmail.com>
2017-11-20 17:41:14 +0000

    post release commit (bump snapshot version)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-bom "0.1.2-SNAPSHOT"
+(defproject clj-bom "0.1.3-SNAPSHOT"
   :description "BOM (Byte-Order-Mark) reader/writer for Clojure"
   :url "https://github.com/jimpil/clj-bom"
   :license {:name "Eclipse Public License"
